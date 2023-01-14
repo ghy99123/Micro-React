3e94279309b809c55534272029189c9bc8e4438e
ghy99123 <2941879036@qq.com>
2023-01-14 22:13:11 +1100

    feat: render API

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-import { createElement } from "./micro-react";
+import { createElement, render, } from "./micro-react";
 
 const element = createElement(
   "h1",
   { id: "element", class: "element" },
   createElement("span", null, "I am span 1"),
-  createElement("span", null, "I am span 2"),
+  createElement("div", {style: 'color: red'}, "I am span 2"),
   "some string"
 );
 
 console.log(element);
+
+
+render(element, document.getElementById('root'));
---
+++ micro-react/render.js
@@ -0,0 +1,14 @@
+export function render(element, container) {
+  const dom =
+    element.type === "TEXT_ELEMENT"
+      ? document.createTextNode("")
+      : document.createElement(element.type);
+
+  Object.keys(element.props)
+    .filter((key) => key !== "children")
+    .forEach((name) => (dom[name] = element.props[name]));
+
+  element.props.children.forEach((child) => render(child, dom));
+
+  container.appendChild(dom);
+}
