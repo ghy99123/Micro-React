3c4b071b7c474621778f2bdcc4b67b0ea22b13d0
ghy99123 <2941879036@qq.com>
2023-01-16 22:25:01 +1100

    feat: add initial concurrent mode code

--- micro-react/render.js
+++ micro-react/render.js
@@ -12,3 +12,26 @@ export function render(element, container) {
 
   container.appendChild(dom);
 }
+
+// The recursive call of render function above will never stop until we have rendered the
+// complete element tree. To avoid blocking the main thread for too long, the rendering
+// work should be able to be interrupted when high priority work needs to be done.
+
+let nextUnitOfWork = null;
+
+// will be run when the main thread is idle
+function workLoop(deadline) {
+  let shouldYield = false;
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    // check how much idle time we have
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+  requestIdleCallback(workLoop);
+}
+
+requestIdleCallback(workLoop);
+
+function performUnitOfWork(nextUnitOfWork) {
+  // TODO: fiber
+}
